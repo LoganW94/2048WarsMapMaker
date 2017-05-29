d373333cc3006fb8e1a14cfeedd2cf707aa1b6b9
LoganW94 <loganwogoman@gmail.com>
2017-05-28 23:26:58 -0400

    complete rework, and error message added

--- MapMaker/screen.py
+++ MapMaker/screen.py
@@ -2,16 +2,27 @@ import pygame
 
 class Screen:
 
-	def new(width, height, color):
+	@staticmethod
+	def new(width, height, color, caption='', imagepath=None):
 
 		screen = pygame.display.set_mode((width, height))
-
-		pygame.display.set_caption('MapMaker Alpha')
-
-		icon = pygame.image.load('assets/icon2.png')
-
-		pygame.display.set_icon(icon)
-
+		pygame.display.set_caption(caption)
+		try:
+			icon = pygame.image.load(imagepath)
+			pygame.display.set_icon(icon)
+		except:
+			Screen.error_msg(screen, "no image")
 		screen.fill(color)
-
 		return screen
+
+
+	def error_msg(_screen, _txt):
+
+		_color = (255,0,0)
+		_x = 5
+		_y = 5
+		_font = pygame.font.SysFont(None, 15)
+
+		screen_text = _font.render(_txt, True, _color)
+		_screen.blit(screen_text, [_x, _y])
+		
