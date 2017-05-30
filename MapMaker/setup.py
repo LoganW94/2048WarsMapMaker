2ca28aa54d27f49ae4dcea0bd252780724766ca7
LoganW94 <loganwogoman@gmail.com>
2017-05-29 23:27:06 -0400

    setup now works, executables can be made and distributed

--- MapMaker/setup.py
+++ MapMaker/setup.py
@@ -1,9 +1,14 @@
+import sys
 from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+	base = "Win32GUI"
 
 setup(
 	name="2048 Wars Map Maker",
 	version='v0.2',
 	options={"build_exe":{"packages":["pygame"],"include_files":["assets/icon.png"]}},
 	description="Map maker for 2048 Wars game for android",
-	executables = [Executable("main.py")]
+	executables = [Executable("main.py", base = base)]
 	)
